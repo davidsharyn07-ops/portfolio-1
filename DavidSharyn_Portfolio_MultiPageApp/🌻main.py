9bbdb0f7c3e2016faf53952f97df04de14388f0b
davidsharyn07-ops <davidsharyn07@gmail.com>
2026-04-25 19:05:07 +0800

    Add files via upload

---
+++ DavidSharyn_Portfolio_MultiPageApp/🌻main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="David Portfolio",
+    page_icon="🌻",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("🌻This is my Portfolio")
+st.markdown("""
+Take an insight of my innovation huub!
+
+guide side bar to know me more:
+- 👩🏻‍💼 About Me
+- 💻 Projects
+- 🤓 SKills
+- 💌 Contact
+""")
+
+st.info("This app shows my skills and information about python and others.")
+
